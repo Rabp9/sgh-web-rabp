a165b882c796f2c44912421a15107cf4130ae2cd
essalud <essalud@essaludrall>
2013-11-05 09:28:27 -0500

    100% arreglado reporte de Pacientes

--- src/java/org/sghweb/beans/ReportePacienteBean.java
+++ src/java/org/sghweb/beans/ReportePacienteBean.java
@@ -14,28 +14,27 @@ import com.lowagie.text.FontFactory;
 import com.lowagie.text.HeaderFooter;
 import com.lowagie.text.Image;
 import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 import java.awt.Color;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletContext;
-import org.apache.commons.io.IOUtils;
 import org.primefaces.event.SelectEvent;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 import org.sghweb.controllers.VwReportepacienteJpaController;
-import org.sghweb.jpa.Paciente;
 import org.sghweb.jpa.VwReportepaciente;
 
 /**
@@ -50,7 +49,7 @@ public class ReportePacienteBean implements Serializable {
     private VwReportepaciente selectedReportePaciente;
     private VwReportepacienteJpaController vrjc;
     private String dni;
-    private StreamedContent reporte;  
+    private StreamedContent reporte;
 
     public ReportePacienteBean() {
         vrjc = new VwReportepacienteJpaController(null, null);
@@ -101,17 +100,102 @@ public class ReportePacienteBean implements Serializable {
     }
 
     public void reportarPaciente() throws IOException, BadElementException, DocumentException {
-        Document document = new Document();
+        Document pdf = new Document();
         ByteArrayOutputStream os = new ByteArrayOutputStream();
-        PdfWriter.getInstance(document, os);
-        document.open();
+        PdfWriter.getInstance(pdf, os);   
+        ServletContext servletContext = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
+        pdf.open();
         
         // INICIO Escritura de Reporte
-        document.add(new Paragraph("A Hello World PDF document."));
+        
+        // Cabecera
+        Paragraph cabecera = new Paragraph();
+        String logo = servletContext.getRealPath("") + File.separator + "resources"+ File.separator + "img" + File.separator + "logo - header.png";
+        cabecera.add(Image.getInstance(logo));
+        pdf.setHeader(new HeaderFooter(cabecera, false));
+        
+        // Título
+        pdf.add(cabecera);
+        Paragraph titulo = new Paragraph("Reporte de Paciente", FontFactory.getFont(FontFactory.HELVETICA, 22, Font.BOLD, new Color(0, 0, 0)));
+        titulo.setAlignment(Element.ALIGN_CENTER);
+        pdf.add(titulo);
+        
+        //Mostrar información de Paciente
+        
+        VwReportepaciente vwReportepaciente = vrjc.findVwReportepaciente(dni);
+        if(vwReportepaciente == null) {
+            System.out.println("dasasddsadasdasas");
+            FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, "No existe Pacietne con DNI: " + dni, null);
+            FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+            return;
+        }
+        pdf.add(Chunk.NEWLINE);
+        PdfPTable table = new PdfPTable(2);
+        
+        PdfPCell cell1 = new PdfPCell(new Paragraph("Informacion Personal", FontFactory.getFont(FontFactory.HELVETICA, 11, Font.BOLD, new Color(0, 0, 0))));
+        cell1.setColspan(2);
+        table.addCell(cell1);
+        
+        table.addCell("Nombre: ");
+        table.addCell(vwReportepaciente.getNombre());
+        
+        table.addCell("DNI: ");
+        table.addCell(vwReportepaciente.getDni());        
 
+        table.addCell("Genero: ");
+        table.addCell(vwReportepaciente.getGenero());
+                
+        table.addCell("Fecha de Nacimiento: ");
+        table.addCell(String.valueOf(vwReportepaciente.getFechaNacimiento()));
+    
+        PdfPCell cell2 = new PdfPCell(new Paragraph("Informacion General", FontFactory.getFont(FontFactory.HELVETICA, 11, Font.BOLD, new Color(0, 0, 0))));
+        cell2.setColspan(2);
+        table.addCell(cell2);
+    
+        table.addCell("Ubicación de Nacimiento: ");
+        table.addCell(vwReportepaciente.getUbicacionNacimiento());
+        
+        table.addCell("Ubicación Actual: ");
+        table.addCell(vwReportepaciente.getUbicacionActual());
+        
+        table.addCell("Dirección: ");
+        table.addCell(vwReportepaciente.getDireccion());
+        
+        table.addCell("Estado Civil: ");
+        table.addCell(vwReportepaciente.getEstadoCivil());
+        
+        PdfPCell cell3 = new PdfPCell(new Paragraph("Informacion de Historia Clínica", FontFactory.getFont(FontFactory.HELVETICA, 11, Font.BOLD, new Color(0, 0, 0))));
+        cell3.setColspan(2);
+        table.addCell(cell3);
+        
+        table.addCell("Numero de Registro: ");
+        table.addCell(vwReportepaciente.getNumeroRegistro());
+        
+        table.addCell("Autogenerado: ");
+        table.addCell(vwReportepaciente.getAutogenerado());
+        
+        table.addCell("Tipo de Asegurado: ");
+        table.addCell(vwReportepaciente.getTipoAsegurado());
+        
+        table.addCell("Titular: ");
+        table.addCell(vwReportepaciente.getTitular());
+                
+        table.addCell("Fecha afiliación: ");
+        table.addCell(String.valueOf(vwReportepaciente.getFechaInicio()));
+        
+        table.addCell("Fecha de ultima cita: ");
+        table.addCell(String.valueOf(vwReportepaciente.getFechaUltimaCita()));
+        
+        table.addCell("Citas del ultimo mes: ");
+        table.addCell(String.valueOf(vwReportepaciente.getNumCitasUltimoMes()));
+        
+        table.addCell("Total de citas: ");
+        table.addCell(String.valueOf(vwReportepaciente.getNumCitasTotal()));
+        
+        pdf.add(table);
         //Fin Escritura de Reprote
         
-        document.close(); // no need to close PDFwriter?
+        pdf.close(); // no need to close PDFwriter?
         InputStream is = new ByteArrayInputStream(os.toByteArray());
         setReporte(new DefaultStreamedContent(is, "application/pdf", "ohyeah.pdf"));
     }
