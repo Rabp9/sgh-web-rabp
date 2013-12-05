104953074d0ca6195d7845e5762ee006e33f81ba
essalud <essalud@essaludrall>
2013-12-05 16:03:20 -0500

    arreglado cu 3

--- src/java/org/sghweb/beans/ReporteRecetaBean.java
+++ src/java/org/sghweb/beans/ReporteRecetaBean.java
@@ -3,9 +3,14 @@ package org.sghweb.beans;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.SelectEvent;
 import org.sghweb.controllers.VwReportepacienteJpaController;
 import org.sghweb.jpa.VwReportepaciente;
 
@@ -14,12 +19,15 @@ import org.sghweb.jpa.VwReportepaciente;
  * @author essalud
  */
 @ManagedBean
-@RequestScoped
+@ViewScoped
+
 public class ReporteRecetaBean implements Serializable  {
 
     private List<VwReportepaciente> listaReportePacientes;
     private VwReportepacienteJpaController vrjc;
     private VwReportepaciente vwReportepaciente;
+    private Date fechaInicio;
+    private Date fechaFin;
 
     public ReporteRecetaBean() {
         vrjc = new VwReportepacienteJpaController(null, null);
@@ -45,7 +53,18 @@ public class ReporteRecetaBean implements Serializable  {
     public List<VwReportepaciente> getListaReportePacientes() {
         return listaReportePacientes;
     }
-
+         
+    public void onRowSelect(SelectEvent event) {
+        vwReportepaciente = (VwReportepaciente) event.getObject();
+    }
+    
+    public void mostrarRecetas() {
+        if(fechaInicio.after(fechaFin)) {
+            FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, "La fecha de Inicio debe ser antes de la Fecha de Fin", null);
+            FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+        }
+    }
+    
     public void setListaReportePacientes(List<VwReportepaciente> listaReportePacientes) {
         this.listaReportePacientes = listaReportePacientes;
     }
@@ -64,5 +83,21 @@ public class ReporteRecetaBean implements Serializable  {
 
     public void setVwReportepaciente(VwReportepaciente vwReportepaciente) {
         this.vwReportepaciente = vwReportepaciente;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
     }
 }
